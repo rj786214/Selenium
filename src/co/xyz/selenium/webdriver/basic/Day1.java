cd501d1bc2d21e4a592e46c4b033a6eff24d69c6
Raj Patel <rj786214@gmail.com>
2023-03-08 17:30:06 -0500

    Small Changes

--- src/co/xyz/selenium/webdriver/basic/Day1.java
+++ src/co/xyz/selenium/webdriver/basic/Day1.java
@@ -43,28 +43,5 @@ public class Day1 {
 
 
 
-//	@Test
-	/*public void invokeBrowser1() {
-		try {
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\rj786\\Downloads\\chromedriver_win32\\chromedriver.exe");
-			driver = new ChromeDriver();
-			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
 
-			driver.get("https://www.facebook.com");
-			driver.findElement(By.name("email")).sendKeys("rj786214@gmail.com");
-			driver.findElement(By.name("pass")).sendKeys("rj786214@gmail.com");
-			driver.findElement(By.name("login")).click();
-			Thread.sleep(5000);
-			driver.quit();
-			System.out.println(driver.getTitle());
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-*/
 }
